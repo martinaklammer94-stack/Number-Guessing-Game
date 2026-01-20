4af792e2b8f763d122c66d15564c0d6f66dc53cc
martina-klammer <martina.klammer94@gmail.com>
2026-01-20 13:12:00 +0100

    Java Script resetGame eingebaut

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 let numberToGuess = Math.round(Math.random() * 10);
-
 let tries = 0;
 
 function guessTheNumber() {
-    tries = tries +1;
+    tries = tries + 1;
     displayTries.innerHTML = "Versuche: " + tries;
 
-    if(numberToGuess == myNumber.value) {
+    if (numberToGuess == myNumber.value) {
         headline.innerHTML = "Glückwunsch! 😁 Du hast gewonnen! 🙌";
+
+        let jsConfetti = new JSConfetti();
+        jsConfetti.addConfetti();
+
+        resetGame();
+        return; 
     }
 
-     if(numberToGuess > myNumber.value) {
+    if (numberToGuess > myNumber.value) {
         headline.innerHTML = "Deine Zahl ist zu klein... 😔";
     }
 
-    if(numberToGuess < myNumber.value) {
+    if (numberToGuess < myNumber.value) {
         headline.innerHTML = "Deine Zahl ist zu groß... 😔";
     }
-    myNumber.value = ' '
+
+    myNumber.value = '';
+}
+
+function resetGame() {
+    numberToGuess = Math.round(Math.random() * 10);
+    tries = 0;
+    displayTries.innerHTML = "Versuche: 0";
+    myNumber.value = '';
 }
