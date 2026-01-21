f79f55b2ea7d9619e676a8aaa8909a7d23cedf68
Max Klammer <maximilian.klammer@gmail.com>
2026-01-21 10:02:20 +0100

    Added some fixes to the guessing game

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 let numberToGuess = Math.round(Math.random() * 10);
 let tries = 0;
 
+const form = document.querySelector("#guessForm");
+const headline = document.querySelector("#headline");
+
 function guessTheNumber() {
-    tries = tries + 1;
-    displayTries.innerHTML = "Versuche: " + tries;
+  let myNumber = document.querySelector("#guessedNumber").value;
+  tries = tries + 1;
+  displayTries.innerHTML = "Versuche: " + tries;
 
-    if (numberToGuess == myNumber.value) {
-        headline.innerHTML = "Glückwunsch! 😁 Du hast gewonnen! 🙌";
+  if (numberToGuess == myNumber.value) {
+    headline.innerHTML = "Glückwunsch! 😁 Du hast gewonnen! 🙌";
 
-        let jsConfetti = new JSConfetti();
-        jsConfetti.addConfetti();
+    let jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti();
 
-        resetGame();
-        return; 
-    }
+    resetGame();
+    return;
+  }
 
-    if (numberToGuess > myNumber.value) {
-        headline.innerHTML = "Deine Zahl ist zu klein... 😔";
-    }
+  if (numberToGuess > myNumber.value) {
+    headline.innerHTML = "Deine Zahl ist zu klein... 😔";
+  }
 
-    if (numberToGuess < myNumber.value) {
-        headline.innerHTML = "Deine Zahl ist zu groß... 😔";
-    }
+  if (numberToGuess < myNumber.value) {
+    headline.innerHTML = "Deine Zahl ist zu groß... 😔";
+  }
 
-    myNumber.value = '';
+  myNumber.value = "";
 }
 
 function resetGame() {
-    numberToGuess = Math.round(Math.random() * 10);
-    tries = 0;
-    displayTries.innerHTML = "Versuche: 0";
-    myNumber.value = '';
+  numberToGuess = Math.round(Math.random() * 10);
+  tries = 0;
+  displayTries.innerHTML = "Versuche: 0";
+  myNumber.value = "";
 }
+
+function submitHandler(event) {
+  event.preventDefault();
+  guessTheNumber();
+}
+
+form.addEventListener("submit", submitHandler);
