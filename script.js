8788f0bba538d2fe090b9fbb0b8d07cbf49a338d
martina-klammer <martina.klammer94@gmail.com>
2026-01-22 15:50:16 +0100

    fixed some bugs
    game works correctly now: tries countig goes up, function works and displays right headline. Still want to work on a restart button.

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
-let numberToGuess = Math.round(Math.random() * 10);
+let numberToGuess = Math.round(Math.random() * 10) + 1;
 let tries = 0;
+let myNumber = Number(document.querySelector("#guessedNumber").value);
 
 const form = document.querySelector("#guessForm");
 const headline = document.querySelector("#headline");
+const displayTries = document.querySelector("#displayTries");
 
 function guessTheNumber() {
-  let myNumber = document.querySelector("#guessedNumber").value;
-  tries = tries + 1;
+  const input = document.querySelector("#guessedNumber");
+  let myNumber = +input.value; // in Zahl konvertieren
+
+  tries++;
   displayTries.innerHTML = "Versuche: " + tries;
 
-  if (numberToGuess == myNumber.value) {
-    headline.innerHTML = "Glückwunsch! 😁 Du hast gewonnen! 🙌";
+  if (myNumber < 1 || myNumber > 10) {
+    headline.innerHTML = "Bitte gib eine Zahl zwischen 1 und 10 ein!";
+    input.value = "";
+    return;
+  }
 
+  if (numberToGuess === myNumber) {
+    headline.innerHTML = "Glückwunsch! 😁 Du hast gewonnen! 🙌";
     let jsConfetti = new JSConfetti();
     jsConfetti.addConfetti();
+
+    setTimeout(resetGame, 50000);
 
     resetGame();
     return;
   }
 
-  if (numberToGuess > myNumber.value) {
+  if (numberToGuess > myNumber) {
     headline.innerHTML = "Deine Zahl ist zu klein... 😔";
   }
 
-  if (numberToGuess < myNumber.value) {
+  if (numberToGuess < myNumber) {
     headline.innerHTML = "Deine Zahl ist zu groß... 😔";
   }
 
-  myNumber.value = "";
+  input.value = "";
 }
 
 function resetGame() {
   numberToGuess = Math.round(Math.random() * 10);
   tries = 0;
+
   displayTries.innerHTML = "Versuche: 0";
-  myNumber.value = "";
+  document.querySelector("#guessedNumber").value = "";
 }
 
 function submitHandler(event) {
